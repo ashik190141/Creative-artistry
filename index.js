eb2f37335b92bf0eac06e50d76e4520bbf815705
ashik190141 <ashanmahmudashik@gmail.com>
2023-08-10 18:03:14 +0600

    finished

---
+++ index.js
@@ -0,0 +1,4 @@
+document.getElementById('menu-btn').addEventListener('click', function () {
+    const mobile_menu_list = document.getElementById('mobile-menu');
+    mobile_menu_list.classList.toggle('hidden')
+})
